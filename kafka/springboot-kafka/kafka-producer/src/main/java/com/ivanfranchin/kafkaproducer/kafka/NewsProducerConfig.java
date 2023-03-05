99e4344b63eb289629bad4d040c0ef65d47a4e6e
Ivan Franchin <ivan.franchin@takeaway.com>
2023-03-05 14:04:56 +0100

    code adjustments in spring-boot kafka producer

--- kafka/springboot-kafka/kafka-producer/src/main/java/com/ivanfranchin/kafkaproducer/kafka/NewsProducerConfig.java
+++ kafka/springboot-kafka/kafka-producer/src/main/java/com/ivanfranchin/kafkaproducer/kafka/NewsProducerConfig.java
@@ -1,15 +1,12 @@
 package com.ivanfranchin.kafkaproducer.kafka;
 
 import com.ivanfranchin.kafkaproducer.domain.News;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
@@ -33,18 +30,5 @@ public class NewsProducerConfig {
     @Bean
     public KafkaTemplate<String, News> kafkaTemplate(ProducerFactory<String, News> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
-    }
-
-    @Bean
-    public KafkaAdmin kafkaAdmin(@Value("${spring.kafka.bootstrap-servers}") String bootstrapServers) {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        return new KafkaAdmin(configs);
-    }
-
-    @Bean
-    public NewTopic newTopic(@Value("${spring.kafka.producer.topic}") String topic,
-                             @Value("${spring.kafka.producer.num-partitions}") Integer numPartitions) {
-        return new NewTopic(topic, numPartitions, (short) 1);
     }
 }
