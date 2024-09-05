a617e4c519054858100ee161ac458534cba6b67d
Ivan Franchin <ivan.franchin@takeaway.com>
2024-09-05 16:36:43 +0200

    Fix flood stage disk watermark issue in Elasticsearch

--- elasticsearch/init-es-indexes.sh
+++ elasticsearch/init-es-indexes.sh
@@ -13,11 +13,15 @@ echo
 
 echo "Fix High Disk Watermark"
 echo "-----------------------"
-# Reference: https://www.elastic.co/guide/en/elasticsearch/reference/current/fix-watermark-errors.html
+# Reference: https://stackoverflow.com/questions/63880017/elasticsearch-docker-flood-stage-disk-watermark-95-exceeded
 
-curl -X PUT localhost:9200/_cluster/settings \
+curl -X PUT  http://localhost:9200/_cluster/settings \
   -H "Content-Type: application/json" \
-  -d '{ "persistent": { "cluster.routing.allocation.disk.watermark.low": "90%", "cluster.routing.allocation.disk.watermark.low.max_headroom": "100GB", "cluster.routing.allocation.disk.watermark.high": "95%", "cluster.routing.allocation.disk.watermark.high.max_headroom": "20GB", "cluster.routing.allocation.disk.watermark.flood_stage": "97%", "cluster.routing.allocation.disk.watermark.flood_stage.max_headroom": "5GB", "cluster.routing.allocation.disk.watermark.flood_stage.frozen": "97%", "cluster.routing.allocation.disk.watermark.flood_stage.frozen.max_headroom": "5GB" } }'
+  -d '{ "transient": { "cluster.routing.allocation.disk.threshold_enabled": false } }'
+
+curl -X PUT http://localhost:9200/_all/_settings \
+  -H "Content-Type: application/json" \
+  -d '{ "index.blocks.read_only_allow_delete": null }'
 
 echo
 echo "Creating indexes"
